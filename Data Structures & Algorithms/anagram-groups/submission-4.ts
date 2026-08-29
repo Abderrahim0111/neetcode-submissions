76fbb64e85493bfe082bf758a648f5d4fbc78fb5
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-08-29 15:59:59 +0100

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const map = {}
+        for(const str of strs) {
+            const key = str.split("").sort().join()
+            if(!map[key]){
+                map[key] = []
+            }
+            map[key].push(str)
+        }
+        return Object.values(map)
+    }
+}
