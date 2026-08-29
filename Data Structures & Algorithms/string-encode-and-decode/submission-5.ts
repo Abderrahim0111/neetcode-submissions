1e4d397b60bd9eb20d7edbd220874a4a5f3cce11
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-08-29 16:49:20 +0100

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let res = ""
+        for(const str of strs){
+            res += str.length + "#" + str
+        }
+        return res
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const res = []
+        let i = 0
+        while(i<str.length){
+            let j = i
+            while(str[j] !== "#"){
+                j++
+            }
+            const length = Number(str.slice(i, j))
+            i = j + 1
+            res.push(str.slice(i, length + i))
+            i += length
+        }
+        return res
+    }
+}
