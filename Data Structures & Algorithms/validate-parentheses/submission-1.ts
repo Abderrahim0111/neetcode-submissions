72bc2801462db27e672fec14d4170a06c1fef7ae
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-01 09:17:14 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const stack = []
+        const map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        for(const c of s){
+            if(map[c]){
+                if(stack[stack.length - 1] === map[c]){
+                    stack.pop()
+                }else{
+                    return false
+                }
+            }else{
+                stack.push(c)
+            }
+        }
+        return stack.length === 0
+    }
+}
