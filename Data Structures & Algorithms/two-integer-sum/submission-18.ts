e1aafcaac518339167c1fa89d6f19c4cfd58044d
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-11 06:50:08 +0100

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = new Map<number, number>()
+        for(let i=0; i<nums.length; i++){
+            map.set(nums[i], i)
+        }
+        for(let i=0; i<nums.length; i++){
+            const diff = target - nums[i]
+            if(map.get(diff) && i !== map.get(diff)){
+                return [i, map.get(diff)]
+            }
+        }
+        return []
+    }
+}
