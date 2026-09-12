7b13e46e7a22dab5ae4cacc30ba6e4297a7f42d8
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-12 10:36:22 +0100

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        const map = new Map()
+        let res=0
+        let l=0
+        let maxFreq = 0
+        for(let r=0; r<s.length; r++){
+            map.set(s[r], (map.get(s[r]) || 0) + 1)
+            maxFreq = Math.max(maxFreq, map.get(s[r]))
+            if((r-l+1) - maxFreq > k){
+                map.set(s[l], map.get(s[l]) - 1)
+                l++
+            }
+            res = Math.max(res, r-l+1)
+        }
+        return res
+    }
+}
