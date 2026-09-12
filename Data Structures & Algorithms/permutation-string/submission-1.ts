80485b6636adf9dc3da437dd52c713033b810ce2
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-12 11:18:48 +0100

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1: string, s2: string): boolean {
+        const count1 = Array(26).fill(0)
+        const count2 = Array(26).fill(0)
+
+        for(let i=0; i<s1.length; i++){
+            count1[s1.charCodeAt(i) - 97]++
+            count2[s2.charCodeAt(i) - 97]++
+        }
+        if(count1.every((val, i) => val === count2[i])) return true
+
+        let l=0
+        for(let r=s1.length; r<s2.length; r++){
+            count2[s2.charCodeAt(r) - 97]++
+            count2[s2.charCodeAt(l) - 97]--
+            l++
+            if(count1.every((val, i) => val === count2[i])) return true
+        }
+        return false
+    }
+}
