e4afc7bb3fe1ff9449883c8c2a86f6a6a776c99c
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-17 10:21:16 +0100

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s: string, t: string): string {
+        const need = new Map()
+        for(let i=0; i<t.length; i++){
+            need.set(t[i], (need.get(t[i]) || 0) + 1)
+        }
+        let haveCount = 0
+        let needCount = need.size
+
+        let res = []
+        let resLength = Infinity
+
+        const window = new Map()
+        let l=0
+        for(let r=0; r<s.length; r++){
+            window.set(s[r], (window.get(s[r]) || 0) + 1)
+            if(window.get(s[r]) === need.get(s[r])){
+                haveCount++
+            }
+            while(haveCount === needCount){
+                if((r-l+1) < resLength){
+                    res = [l, r]
+                    resLength = r-l+1
+                }
+                window.set(s[l], window.get(s[l]) - 1)
+                if(window.get(s[l]) < need.get(s[l])){
+                    haveCount--
+                }
+                l++
+            }
+        }
+        if(resLength !== Infinity){
+            return s.slice(res[0], res[1] + 1)
+        }
+        return ""
+    }
+}
