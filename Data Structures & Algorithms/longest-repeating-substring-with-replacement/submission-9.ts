f19c64b07b9de7b2e834098cfbcefe085ce16b0e
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-23 12:19:35 +0100

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        let res = 0
+        let freq = 0
+        const map = new Map()
+        let l=0
+        for(let r=0; r<s.length; r++){
+            map.set(s[r], (map.get(s[r]) || 0) + 1)
+            freq = Math.max(freq, map.get(s[r]))
+            if((r-l+1) - freq > k){
+                map.set(s[l], map.get(s[l]) - 1)
+                l++
+            }
+            res = Math.max(res, r-l+1)
+        }
+        return res
+    }
+}
