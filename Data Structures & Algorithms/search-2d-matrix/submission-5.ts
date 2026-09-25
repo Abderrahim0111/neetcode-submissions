775df3e1f573794f3c527e22dc86c22cdabcf516
abderrahimdefaoui <142720321+Abderrahim0111@users.noreply.github.com>
2026-09-25 15:34:21 +0100

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix: number[][], target: number): boolean {
+        const rows = matrix.length
+        const cols = matrix[0].length
+        let l = 0
+        let r = rows*cols-1
+        while(l<=r){
+            const mid = Math.floor((l+r)/2)
+            const row = Math.floor(mid/cols)
+            const col = mid%cols
+            const val = matrix[row][col]
+            if(val < target){
+                l = mid + 1
+            }else if(val > target){
+                r = mid - 1
+            }else{
+                return true
+            }
+        }
+        return false
+    }
+}
